282d4697dc2f1a1f7a6c53d27c1e186390a5e548
Ankit <ankitsharma.as420@gmail.com>
2024-05-28 23:32:07 +0530

    first commit

---
+++ src/test/java/com/Blogging/Platform/Blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Blogging.Platform.Blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
