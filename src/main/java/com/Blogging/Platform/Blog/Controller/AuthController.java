a142e521a10bbf0f789f6bf4dfbba22221220c91
Ankit <ankitsharma.as420@gmail.com>
2024-06-03 15:43:52 +0530

    added logout controller api to logout

--- src/main/java/com/Blogging/Platform/Blog/Controller/AuthController.java
+++ src/main/java/com/Blogging/Platform/Blog/Controller/AuthController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import com.Blogging.Platform.Blog.Auth.AuthService;
 import com.Blogging.Platform.Blog.Auth.AuthenticationResponse;
 import com.Blogging.Platform.Blog.Model.AuthenticationRequest;
 import com.Blogging.Platform.Blog.Model.RegisterRequest;
+import com.Blogging.Platform.Blog.Service.TokenBlacklistService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,6 +27,9 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
+    @Autowired
+    private TokenBlacklistService tokenBlacklistService;
+
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest registerRequest) {
         AuthenticationResponse authResponse = authService.register(registerRequest);
@@ -37,6 +42,16 @@ public class AuthController {
         AuthenticationResponse response = authService.authenticate(request);
         logger.info("Authentication done successfully for email: {}", request.getEmail());
         return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("/logout")
+    public String logout(@RequestHeader("Authorization") String tokenHeader) {
+        if (tokenHeader != null && tokenHeader.startsWith("Bearer ")) {
+            String token = tokenHeader.substring(7);
+            tokenBlacklistService.blacklistToken(token);
+            return "Logged out successfully";
+        }
+        return "Invalid token";
     }
 
 }
