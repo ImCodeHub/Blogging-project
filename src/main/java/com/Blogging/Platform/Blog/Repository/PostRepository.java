7823398ce94b6e026061c830457f3ee1c829b7a4
Ankit <ankitsharma.as420@gmail.com>
2024-06-03 17:43:16 +0530

    added new method to find post by id and author by id to find the post.

--- src/main/java/com/Blogging/Platform/Blog/Repository/PostRepository.java
+++ src/main/java/com/Blogging/Platform/Blog/Repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.Blogging.Platform.Blog.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,5 +12,5 @@ import com.Blogging.Platform.Blog.Entity.Post;
 public interface PostRepository extends JpaRepository<Post, Integer> {
 
     List<Post> findByAuthorId(Integer authorId);
-    List<Post> findByPostIdAndAuthorId(Integer postId, Integer authorId);
+    Optional<Post> findPostByIdAndAuthor_Id(Integer postId, Integer authorId);
 }
