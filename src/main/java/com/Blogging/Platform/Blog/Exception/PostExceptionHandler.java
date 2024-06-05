f350c1c4c5be22e4d682594d01b20e4849373897
Ankit <ankitsharma.as420@gmail.com>
2024-06-05 23:15:31 +0530

    created exceptiona handling methods

--- src/main/java/com/Blogging/Platform/Blog/Exception/PostExceptionHandler.java
+++ src/main/java/com/Blogging/Platform/Blog/Exception/PostExceptionHandler.java
@@ -6,9 +6,12 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-
+import com.Blogging.Platform.Blog.Exception.CustomException.AuthorNotFoundException;
+import com.Blogging.Platform.Blog.Exception.CustomException.EmailAlreadyExistException;
+import com.Blogging.Platform.Blog.Exception.CustomException.InvalidEmailException;
 import com.Blogging.Platform.Blog.Exception.CustomException.PostNotFoundException;
 
+// to hange the exception
 @ControllerAdvice
 public class PostExceptionHandler {
     @ExceptionHandler(PostNotFoundException.class)
@@ -16,5 +19,18 @@ public class PostExceptionHandler {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.OK);
     }
 
-    
+    @ExceptionHandler(AuthorNotFoundException.class)
+    public ResponseEntity<?> handleAuthorNotFoundException(AuthorNotFoundException ex, WebRequest webRequest){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.OK);
+    }
+
+    @ExceptionHandler(InvalidEmailException.class)
+    public ResponseEntity<?> handleInvalidEmailException(InvalidEmailException ex, WebRequest webRequest){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.OK);
+    }
+
+    @ExceptionHandler(EmailAlreadyExistException.class)
+    public ResponseEntity<?> handleEmailAlreadyExistException(EmailAlreadyExistException ex, WebRequest webRequest){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.OK);
+    }   
 }
