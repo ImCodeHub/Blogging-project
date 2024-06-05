4a39fcefc114d59e046ee0fde179d00cbf458e1b
Ankit <ankitsharma.as420@gmail.com>
2024-06-06 00:02:31 +0530

    added Role variable in methor parameter

--- src/main/java/com/Blogging/Platform/Blog/Repository/UserRepository.java
+++ src/main/java/com/Blogging/Platform/Blog/Repository/UserRepository.java
@@ -17,5 +17,7 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     @Query(value = "SELECT post.author AS author_id, CONCAT (user.first_name,' ',user.last_name) AS NAME , user.email, COUNT(post.author) AS blog_count FROM post INNER JOIN USER ON post.author = user.id GROUP BY user.id, user.first_name", nativeQuery = true)
     List<Object[]> findAllAuthorsWithPostCountNative();
 
+    Optional<User> findByIdAndRole(Integer authorId, Role role );
+
     List<User> findByRole(Role role);
 }
