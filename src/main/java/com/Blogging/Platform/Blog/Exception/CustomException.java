2847c3ff9d6bf304158bc0d39b3d1c1de09b7343
Ankit <ankitsharma.as420@gmail.com>
2024-06-05 23:07:37 +0530

    created customer exception classes

--- src/main/java/com/Blogging/Platform/Blog/Exception/CustomException.java
+++ src/main/java/com/Blogging/Platform/Blog/Exception/CustomException.java
@@ -1,18 +1,32 @@
 package com.Blogging.Platform.Blog.Exception;
 
+// To handle the exception.
 public class CustomException {
-    public static class PostNotFoundException extends RuntimeException{
-        public PostNotFoundException(String message){
+
+    // if post not found.
+    public static class PostNotFoundException extends RuntimeException {
+        public PostNotFoundException(String message) {
             super(message);
         }
     }
-    public static class InvalidEmailException extends RuntimeException{
-        public InvalidEmailException(String message){
+
+    // if author not found.
+    public static class AuthorNotFoundException extends RuntimeException {
+        public AuthorNotFoundException(String message) {
             super(message);
         }
     }
-    public static class EmailAlreadyExistException extends RuntimeException{
-        public EmailAlreadyExistException(String message){
+
+    // if invalid email found.
+    public static class InvalidEmailException extends RuntimeException {
+        public InvalidEmailException(String message) {
+            super(message);
+        }
+    }
+
+    // if email is already exist.
+    public static class EmailAlreadyExistException extends RuntimeException {
+        public EmailAlreadyExistException(String message) {
             super(message);
         }
     }
