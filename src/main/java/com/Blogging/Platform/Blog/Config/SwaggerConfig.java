27c6bf1a781b7ed404ce267d891018cb704549fe
Ankit <ankitsharma.as420@gmail.com>
2024-06-06 08:45:50 +0530

    created Swagger configuration file

---
+++ src/main/java/com/Blogging/Platform/Blog/Config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package com.Blogging.Platform.Blog.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("Blog post API")
+                .version("1.0")
+                .description("API for posting blog and User controller by Admin")
+                .contact(new Contact()
+                    .name(" for API Support")
+                    .email("ankitsharma.as420@gmail.com")));
+    }
+}
